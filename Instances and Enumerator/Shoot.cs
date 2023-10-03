d6bf396815a2df4c696cbc54fbc33c0289ccee3c
Gustavo Carniel <gustavocarniel@hotmail.com>
2023-10-03 10:42:14 -0300

    Update Shoot.cs

--- Instances and Enumerator/Shoot.cs
+++ Instances and Enumerator/Shoot.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    [SerializeField] GameObject instanceObject; //object to instance
-    [SerializeField] Transform spawnPoint; //point in space to the object spawn
+    [SerializeField] GameObject instanceObject; //object to instance (prefab)
+    [SerializeField] Transform spawnPoint; //point in space for the object spawn
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GameObject shoot = Instantiate(instanceObject, spawnPoint.position, spawnPoint.rotation); //new object
-            shoot.GetComponent<Rigidbody>().AddForce(Vector3.forward * 1000f); //getting the rigidbody from the new instantiate object
-            Destroy(shoot, 2f);
+            GameObject shoot = Instantiate(instanceObject, spawnPoint.position, spawnPoint.rotation); //new gameobject variable
+            shoot.GetComponent<Rigidbody>().AddForce(Vector3.forward * 1000f); //getting the rigidbody from the new instantiated object
+            Destroy(shoot, 2f); //destroy with delay
         }
     }
 }
