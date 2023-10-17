4a98aa247dcfddfb7db0e7e63e8fb949ade6d148
Gustavo Carniel <gustavocarniel@hotmail.com>
2023-10-17 19:18:04 -0300

    raycast updates debugging

--- Plataform and Raycast/PlayerControllerRaycast.cs
+++ Plataform and Raycast/PlayerControllerRaycast.cs
@@ -13,16 +13,18 @@ public class PlayerControllerRaycast : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         //the component Rigidbody2D (which is inside the object this script is attached to) is now available to be accessed from the rb variable
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        isMovingHorizontal = Input.GetAxisRaw("Horizontal"); //Raw goes from 0 to 1 and 0 to -1
+        //Raw goes from 0 to 1 and 0 to -1
+        isMovingHorizontal = Input.GetAxisRaw("Horizontal");
         //Debug.Log(isMovingHorizontal);
 
-        PlayerLooking(isMovingHorizontal); //this function uses isMovingHorizontal as the argument it asks for
+        //this function uses isMovingHorizontal as the argument it asks for
+        PlayerLooking(isMovingHorizontal);
 
         MovePlayer();
 
@@ -51,73 +53,91 @@ public class PlayerControllerRaycast : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (Input.GetKey(KeyCode.E)) //not the actual Raycast just for visualization purposes
+        //not the actual Raycast just for visualization purposes
+        if (Input.GetKey(KeyCode.E))
         {
-            Gizmos.color = Color.red; //first define the color of the gizmo - it need to be fully opaque (1)
+            //first define the color of the gizmo - it need to be fully opaque (1)
+            Gizmos.color = Color.red;
 
             if (isLookingRight == true)
                 Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + interactionDistance, transform.position.y));
 
+            //invert the raycast direction based on the bool variable
             if (isLookingRight == false)
-                Gizmos.DrawLine(transform.position, new Vector2(transform.position.x - interactionDistance, transform.position.y)); //invert the raycast direction based on the bool variable
+                Gizmos.DrawLine(transform.position, new Vector2(transform.position.x - interactionDistance, transform.position.y));
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            UnityEditor.Handles.color = Color.green;
+            UnityEditor.Handles.DrawWireDisc(transform.position, transform.forward, interactionDistance);
         }
     }
 
     void MovePlayer()
     {
+        //velocity is for 2D linear movement, since it is a Vector2. The Y axis keeps the velocity of the Rigidbody2D
         rb.velocity = new Vector2(isMovingHorizontal * speed, rb.velocity.y);
-        //velocity is for 2D movement, since it is a Vector2. The Y axis keeps the velocity of the Rigidbody2D
     }
 
     void JumpPlayer()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce); //add the float value to the Y axis
+            //add the float value to the Y axis
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             Debug.Log("Jump");
         }
     }
 
-    protected void PlayerLooking(float lastMovement) //returns -1, 0 or 1. the variable lastMovement only exists inside this function to save memory
+    //returns -1, 0 or 1. the variable lastMovement only exists inside this function to save memory
+    protected void PlayerLooking(float lastMovement)
     {
         if (lastMovement > 0 && isLookingRight == false)
         {
+            //updates the variable because now it's looking to the right
             this.gameObject.transform.localScale = new Vector3(1, 1, 1);
-            isLookingRight = true; //updates the variable because now it's looking to the right
+            isLookingRight = true;
         }
 
         if (lastMovement < 0 && isLookingRight == true)
         {
-            this.gameObject.transform.localScale = new Vector3(-1, 1, 1); //flips the scale on the X axis
+            //flips the scale on the X axis
+            this.gameObject.transform.localScale = new Vector3(-1, 1, 1);
             isLookingRight = false;
         }
     }
 
-    //shoots a straight line
+    //shoots a straight line and returns only the 1st object it hits
     void RaycastInteraction()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, interactionDistance, 7); //raycast returns the 1st object it hits. 7 is the layer to ignore casting
+        //7 is the layer to ignore casting. no array needed, it stores one value
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, interactionDistance, 7);
 
-        if (hit.transform && hit.transform.gameObject.tag == "Item") //if the first object next to the player exists and the tag is equal to "Item", object will be destroyed
+        //if the first object next to the player exists and the tag is equal to "Item", object will be destroyed
+        if (hit.transform && hit.transform.gameObject.tag == "Item")
         {
             Destroy(hit.transform.gameObject);
         }
     }
-    //
+    //stores in an array all objects that were hitted by the ray
     void MultipleRaycastInteraction()
     {
-        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, transform.right, interactionDistance, 7); //stores all objects that were hitted
+        //stores all objects that were hitted
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, transform.right, interactionDistance);
         Debug.Log(hits.Length);
 
-        foreach (RaycastHit2D hit in hits) //loop for each element in the array
+        //loop for each element in the array
+        foreach (RaycastHit2D hit in hits)
         {
-            if (hit.transform && hit.transform.gameObject.tag == "Item") //destroy the objects with tags and preserve the non tagged
+            //destroy the objects with tags and preserve the non tagged
+            if (hit.transform && hit.transform.gameObject.tag == "Item")
             {
                 Destroy(hit.transform.gameObject);
             }
         }
     }
 
+    //creates a circle around the origin with range and distance from origin. store the casts in an array
     void CircleRaycastInteraction()
     {
         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, interactionDistance, Vector2.zero);
@@ -132,9 +152,10 @@ public class PlayerControllerRaycast : MonoBehaviour
         }
     }
 
+    //uses the Collider class to store the hits. this is a way that uses less memory to store more objects in an array
     void CircleOverlapRaycastInteraction()
     {
-        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, interactionDistance, 7);
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, interactionDistance);
         foreach (Collider2D hit in hits)
         {
             if (hit.transform && hit.transform.gameObject.tag == "Item")
--- Plataform and Raycast/Script Description.md
+++ Plataform and Raycast/Script Description.md
@@ -2,5 +2,5 @@
 The 2D player moves and jumps using the linear velocity of the Rigidbody2D. 
 There is method to determine if the player is looking left or right, so the scale of the x axis can be inverted, changing the orientation of the player's sprite.
 
-For debug purposes, the OnDrawGizmos method was implemented (E) to see how a single raycast would be draw on the screen.
+For debugging purposes, the OnDrawGizmos method was implemented (keys E - Q) to see how the raycast is gonna be draw on the screen by Gizmos (DrawLine) and the Unity's Handles way (DrawWireDisc).
 And finally, there are 4 Raycast interaction methods declared, each one can be called by pressing the keys: R, M, C and O.
